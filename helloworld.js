0ac2444a6677fbb5df99027b361b49431d50fd24
nmadanshah@gmail.com <nmadanshah@gmail.com>
2020-09-22 23:07:20 +0545

    la

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello Bello hi Ciao\n')
+   response.end('<h1>Welcome to the AWS event</h1>\n')
 }).listen(3000)
 
 // Console will print the message
